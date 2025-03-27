a352feb0f47485306647a70bf1e11c9469a076cd
aasami27 <sami.official554@gmail.com>
2025-03-26 22:47:07 -0400

    Implemented Sprint Functionality

--- theta-tau-game/scripts/player.gd
+++ theta-tau-game/scripts/player.gd
@@ -1,33 +1,37 @@
 extends CharacterBody2D
 
-const speed = 100
-const balls = 69
+const SPEED = 100
+const SPRINT_MULTIPLIER = 1.7 # Adjust sprint speed multiplier
 var current_direction = "none"
 
 func _physics_process(delta):
 	player_movement(delta)
 
 func player_movement(delta):
+	var current_speed = SPEED
+	
+	if Input.is_action_pressed("sprint"):  # Sprinting increases speed
+		current_speed *= SPRINT_MULTIPLIER
 	
 	if Input.is_action_pressed("ui_right"):
 		current_direction= "right"
 		play_anim(1)
-		velocity.x = speed
+		velocity.x = current_speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_direction = "left"
 		play_anim(1)
-		velocity.x = -speed
+		velocity.x = -current_speed
 		velocity.y = 0 
 	elif Input.is_action_pressed("ui_down"):
 		current_direction = "down"
 		play_anim(1)
-		velocity.y = speed
+		velocity.y = current_speed
 		velocity.x = 0 
 	elif Input.is_action_pressed("ui_up"):
 		current_direction = "up"
 		play_anim(1)
-		velocity.y = -speed
+		velocity.y = -current_speed
 		velocity.x = 0 
 	else:
 		play_anim(0)
