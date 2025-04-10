af2a87fc1456e9f5b0e11d2a5350adb8d5e138be
meemee72 <jyang222@buffalo.edu>
2025-04-09 22:24:26 -0400

    getting rid of old pause menu

--- theta-tau-game/scripts/world.gd
+++ theta-tau-game/scripts/world.gd
@@ -1,27 +1,8 @@
 extends Node2D
-@onready var pause_menu = $PauseMenu	
-var paused = false
 
 func _ready():
-	pause_menu.hide()
 	if NavigationManager.spawn_door_tag != null:
 		_on_level_spawn(NavigationManager.spawn_door_tag)
-
-func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		pauseMenu() 
-
-func pauseMenu():
-	if (paused):
-		pause_menu.hide()
-		Engine.time_scale=1
-	else:
-		pause_menu.show()
-		Engine.time_scale = 0
-	
-	paused = !paused
-
-
 
 func _on_level_spawn(destination_tag: String):
 	var door_path = "doors/door_" + destination_tag
